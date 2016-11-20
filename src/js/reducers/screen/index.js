2abf248896145485a9f42eda45f9e29047c6ebf2
Nguyễn Tuấn ANh <anhnt.fami@gmail.com>
2016-11-20 17:51:07 +0700

    online

--- src/js/reducers/screen/index.js
+++
@@ -1,8 +0,0 @@
-export default function screen(state = 'menu', action) {
-  switch (action.type) {
-    case 'GO_TO_STORY': return 'story';
-    case 'GO_TO_GAME': return 'game';
-    case 'GO_TO_MENU': return 'menu';
-    default: return state;
-  }
-}
